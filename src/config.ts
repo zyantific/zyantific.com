ad3448ddabe06fc622205435c7e35a45d344b05d
Christian K <ch.kuch43@gmail.com>
2022-07-20 01:23:00 +0200

    feat: added types, site-config

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+import type { NavItems } from './types';
+
+export const NAV_ITEMS: NavItems = {
+	zydis: {
+		name: 'Zydis',
+		path: 'https://zydis.re/',
+	},
+	projects: {
+		name: 'Other Projects',
+		path: '/projects',
+	},
+};
+
+export const SITE = {
+	title: 'zyantific | zyantific team development blog',
+	description: 'zyantific team development blog',
+	url: 'https://zyantific.com',
+	github: 'https://github.com/zyantific',
+	image: 'https://zyantific.com/images/logo.png',
+};
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type NavItems = {
+	[key: string]: NavItem;
+};
+
+export type NavItem = {
+	name: string;
+	path: string;
+};
