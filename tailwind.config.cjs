3561aab943253b4b5279396f3e14ce8b139f2098
Christian K <ch.kuch43@gmail.com>
2022-08-10 21:22:02 +0200

    style: added text-styles for articles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,11 +6,14 @@ module.exports = {
 		colors: {
 			// Brand Colors
 			brand: '#11E2FF',
+			'brand-darker': '#1DABE8',
 			// System Colors
 			red: '#EF4444',
 			green: '#22C55E',
 			blue: '#01D0FF',
 			yellow: '#FFBA08',
+			white: '#FFFFFF',
+			link: '#117EFF',
 			'text-faded': '#9397AD',
 			// Light-Mode Colors
 			'lm-heading': '#131022',
@@ -18,8 +21,9 @@ module.exports = {
 			'lm-card-outline': '#3E4265',
 			'lm-header-text': '#3E4265',
 			'lm-logo': '#3E4265',
-			'lm-body': '#585C7B',
+			'lm-body': '#484D6C',
 			'lm-faded': '#FFFFFF',
+			'lm-code-bg': '#dedede',
 			// Dark-Mode Colors
 			'dm-heading': '#FFFFFF',
 			'dm-card-outline': '#FFFFFF',
@@ -28,14 +32,81 @@ module.exports = {
 			'dm-logo': '#FFFFFF',
 			'dm-body': '#D4D7E5',
 			'dm-faded': '#191E2C',
+			'dm-code-bg': '#191E2C',
+			'dm-article-body': '#babed1',
 		},
 		// fontFamily: {
 		// 	sans: ['ui-sans-serif', 'system-ui'],
 		// },
 		extend: {
-			typography: {
+			typography: ({ theme }) => ({
+				custom: {
+					css: {
+						'--tw-prose-body': theme('colors.lm-body'),
+						'--tw-prose-headings': theme('colors.lm-heading'),
+						'--tw-prose-lead': theme('colors.brand'),
+						'--tw-prose-links': theme('colors.dm-background'),
+						'--tw-prose-bold': theme('colors.lm-heading'),
+						'--tw-prose-counters': theme('colors.lm-body'),
+						'--tw-prose-bullets': theme('colors.lm-body'),
+						'--tw-prose-hr': theme('colors.lm-body'),
+						'--tw-prose-quotes': theme('colors.lm-body'),
+						'--tw-prose-quote-borders': theme('colors.brand-darker'),
+						'--tw-prose-captions': theme('colors.lm-body'),
+						'--tw-prose-code': theme('colors.lm-body'),
+						'--tw-prose-code-bg': theme('colors.lm-code-bg'),
+						'--tw-prose-link-border': theme('colors.brand-darker'),
+						// '--tw-prose-pre-code': theme('colors.orange[100]'),
+						// '--tw-prose-pre-bg': theme('colors.orange[900]'),
+						// '--tw-prose-th-borders': theme('colors.orange[300]'),
+						// '--tw-prose-td-borders': theme('colors.orange[200]'),
+						// '--tw-format-th-bg': theme('colors.orange[50]'),
+					},
+				},
+				'custom-invert': {
+					css: {
+						//copy above but remove "-invert-"
+						'--tw-prose-body': theme('colors.dm-article-body'),
+						'--tw-prose-headings': theme('colors.dm-heading'),
+						'--tw-prose-lead': theme('colors.dm-heading'),
+						'--tw-prose-links': theme('colors.dm-heading'),
+						'--tw-prose-bold': theme('colors.dm-heading'),
+						'--tw-prose-counters': theme('colors.dm-heading'),
+						'--tw-prose-bullets': theme('colors.dm-heading'),
+						'--tw-prose-hr': theme('colors.dm-heading'),
+						'--tw-prose-quotes': theme('colors.dm-article-body'),
+						'--tw-prose-quote-borders': theme('colors.brand'),
+						'--tw-prose-captions': theme('colors.dm-heading'),
+						'--tw-prose-code': theme('colors.text-faded'),
+						'--tw-prose-code-bg': theme('colors.dm-code-bg'),
+						'--tw-prose-link-border': theme('colors.brand'),
+						// '--tw-prose-pre-code': theme('colors.orange[100]'),
+						// '--tw-prose-pre-bg': theme('colors.orange[900]'),
+						// '--tw-prose-th-borders': theme('colors.orange[300]'),
+						// '--tw-prose-td-borders': theme('colors.orange[200]'),
+						// '--tw-format-th-bg': theme('colors.orange[50]'),
+					},
+				},
 				DEFAULT: {
 					css: {
+						a: {
+							// color: theme('colors.white'),
+							fontWeight: 'bold',
+							textDecoration: 'none',
+							borderBottomWidth: '1px',
+							borderBottomStyle: 'solid',
+							borderBottomColor: 'var(--tw-prose-link-border)',
+							'&:hover': {
+								borderBottomWidth: '2px',
+							},
+						},
+						code: {
+							borderRadius: '4px',
+							backgroundColor: 'var(--tw-prose-code-bg)',
+						},
+						hr: {
+							borderColor: 'var(--tw-prose-hr)',
+						},
 						pre: {
 							fontSize: '1rem',
 						},
@@ -69,10 +140,10 @@ module.exports = {
 						},
 					},
 				},
-			},
+			}),
 		},
 	},
-	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/line-clamp'), require('daisyui')],
+	plugins: [require('daisyui'), require('@tailwindcss/typography'), require('@tailwindcss/line-clamp')],
 	daisyui: {
 		themes: [],
 	},
